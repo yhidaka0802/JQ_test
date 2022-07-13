881ba9d6384c3b65ce8c827e25dee2bcd22d6c32
yu.hidaka <light3223b@gmail.com>
2022-07-13 22:17:17 +0900

    first-commit

---
+++ js/main.js
@@ -0,0 +1,37 @@
+'use strict';
+
+let slideImg = $('.slide_imgBox');
+
+function slideShow() {
+  if($(slideImg).hasClass('company')){
+    slideImg.removeClass('company');
+    slideImg.show(1000);
+    slideImg.css('background-image', 'url("/img/news.png")');
+    setTimeout(function(){
+      slideImg.hide(1000);
+    }, 4000);
+    slideImg.addClass('news');
+  }
+
+  else if ($(slideImg).hasClass('news')){
+    slideImg.removeClass('news');
+    slideImg.show(1000);
+    slideImg.css('background-image', 'url("/img/office.png")');
+    setTimeout(function(){
+      slideImg.hide(1000);
+  }, 4000);
+    slideImg.addClass('office');
+  }
+
+  else {
+    slideImg.removeClass('office');
+    slideImg.show(1000);
+    slideImg.css('background-image', 'url("/img/company.png")');
+    setTimeout(function(){
+      slideImg.hide(1000);
+    }, 4000);
+    slideImg.addClass('company');
+  }
+}
+
+setInterval(slideShow, 5000);
